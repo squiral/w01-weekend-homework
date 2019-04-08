ca4108b276f7195eb417e319c4b6ea2556490fe4
Alison Williams <alison.lauriewill@gmail.com>
2019-04-08 09:05:41 +0100

    Actually all completed

--- specs/pet_shop_spec.rb
+++ specs/pet_shop_spec.rb
@@ -201,9 +201,9 @@ class TestPetShop < Minitest::Test
     sell_pet_to_customer(@pet_shop, pet, customer)
 
     assert_equal(0, customer_pet_count(customer))
-  #   assert_equal(0, pets_sold(@pet_shop))
-  #   assert_equal(1000, customer_cash(customer))
-  #   assert_equal(1000, total_cash(@pet_shop))
+    assert_equal(0, pets_sold(@pet_shop))
+    assert_equal(1000, customer_cash(customer))
+    assert_equal(1000, total_cash(@pet_shop))
   end
 
   def test_sell_pet_to_customer__insufficient_funds
